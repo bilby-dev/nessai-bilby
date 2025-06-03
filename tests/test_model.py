e60ea0a0d596d67025b0bf4ce5f6a2d26004e948
Michael J. Williams <michaeljw1@googlemail.com>
2025-06-03 14:24:55 +0100

    TST: set RNG in create_model test (#13)
    * TST: set RNG in create_model test
    
    * TST: use `set_rng`

--- tests/test_model.py
+++ tests/test_model.py
@@ -10,16 +10,18 @@ def ModelClass(request):
     return request.param
 
 
-def test_create_model(ModelClass, bilby_gaussian_likelihood_and_priors):
+def test_create_model(ModelClass, bilby_gaussian_likelihood_and_priors, rng):
     likelihood, priors = bilby_gaussian_likelihood_and_priors
     priors = bilby.core.prior.PriorDict(priors)
     model = ModelClass(priors=priors, likelihood=likelihood)
+    # Do this rather than using `set_rng` to ensure backwards compatibility
+    model.set_rng(rng)
     model.validate_bilby_likelihood()
     model.verify_model()
 
 
 def test_sample_model_with_nessai(
-    bilby_gaussian_likelihood_and_priors, tmp_path, ModelClass
+    bilby_gaussian_likelihood_and_priors, tmp_path, ModelClass,
 ):
     likelihood, priors = bilby_gaussian_likelihood_and_priors
     priors = bilby.core.prior.PriorDict(priors)
