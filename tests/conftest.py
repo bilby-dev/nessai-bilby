732cdb394df2b3a90ded0b702f9ed04b955e301c
Michael J. Williams <michaeljw1@googlemail.com>
2025-10-08 16:39:09 +0100

    TST: add test for n_pool

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,20 @@ import pytest
 from nessai.livepoint import reset_extra_live_points_parameters
 
 
+class GaussianLikelihood(bilby.Likelihood):
+    def __init__(self):
+        """A very simple Gaussian likelihood"""
+        super().__init__(parameters={"x": None, "y": None})
+
+    def log_likelihood(self):
+        """Log-likelihood."""
+        return -0.5 * (
+            self.parameters["x"] ** 2.0 + self.parameters["y"] ** 2.0
+        ) - np.log(2.0 * np.pi)
+
+
 @pytest.fixture
 def bilby_gaussian_likelihood_and_priors():
-    class GaussianLikelihood(bilby.Likelihood):
-        def __init__(self):
-            """A very simple Gaussian likelihood"""
-            super().__init__(parameters={"x": None, "y": None})
-
-        def log_likelihood(self):
-            """Log-likelihood."""
-            return -0.5 * (
-                self.parameters["x"] ** 2.0 + self.parameters["y"] ** 2.0
-            ) - np.log(2.0 * np.pi)
-
     likelihood = GaussianLikelihood()
     priors = dict(
         x=bilby.core.prior.Uniform(-10, 10, "x"),
@@ -34,3 +35,8 @@ def reset_live_point_parameters():
 @pytest.fixture()
 def rng():
     return np.random.default_rng()
+
+
+@pytest.fixture(params=[None, 2])
+def n_pool(request):
+    return request.param
