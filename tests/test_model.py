0778ee22de3097a6772dda16ce542a83363c4032
Michael J. Williams <michaeljw1@googlemail.com>
2026-05-29 15:09:00 +0100

    TST: expand test to include constraints

--- tests/test_model.py
+++ tests/test_model.py
@@ -42,13 +42,7 @@ def test_sample_model_with_nessai(
 
 
 @pytest.mark.parametrize("z", [1.0, bilby.core.prior.DeltaFunction(1.0)])
-def test_model_fixed_keys(bilby_likelihood, z):
-    from bilby.core.prior import PriorDict
-
-    priors = PriorDict()
-    priors["m"] = bilby.core.prior.Uniform(0, 1)
-    priors["c"] = bilby.core.prior.Uniform(0, 1)
-    priors["z"] = z
-
-    model = BilbyModel(priors=priors, likelihood=bilby_likelihood)
+def test_model_fixed_keys(bilby_priors, bilby_likelihood, z):
+    bilby_priors["z"] = z
+    model = BilbyModel(priors=bilby_priors, likelihood=bilby_likelihood)
     assert model.fixed_parameters == {"z": 1.0}
